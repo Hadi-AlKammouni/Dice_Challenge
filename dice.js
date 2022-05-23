241b363293d0f318ea84c90158788e90ed4c4a8d
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-23 10:54:11 +0300

    js// Declaring all the required variables

---
+++ dice.js
@@ -0,0 +1,10 @@
+// Declaring array to pick a random number from it
+const dice_array=[1,2,3,4,5,6]
+
+// Declaring variables to simplify their usage later
+let image_one=document.getElementsByClassName("img1")[0]
+let image_two=document.getElementsByClassName("img2")[0]
+
+// Declaring two variables to store the result of each player
+let player_one = 0
+let player_two= 0
