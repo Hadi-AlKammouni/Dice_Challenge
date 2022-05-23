616f7d000b6cbd5239dd02661691f120b3b6de7b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-23 10:57:30 +0300

    js// Showing the first dice + storing its value

--- dice.js
+++ dice.js
@@ -14,4 +14,40 @@ for (let i=0; i<2; i++){
 
     let random_dice = dice_array[Math.floor(Math.random() * dice_array.length)]
 
+    // First image
+    if (i==0){
+        switch (random_dice){
+
+            case 1:
+                image_one.src="assets/dice1.png"
+                player_one=1
+            break;
+    
+            case 2:
+                image_one.src="assets/dice2.png"
+                player_one=2
+            break;
+    
+            case 3:
+                image_one.src="assets/dice3.png"
+                player_one=3
+            break;
+    
+            case 4:
+                image_one.src="assets/dice4.png"
+                player_one=4
+            break;
+    
+            case 5:
+                image_one.src="assets/dice5.png"
+                player_one=5
+            break;
+    
+            case 6:
+                image_one.src="assets/dice6.png"
+                player_one=6
+            break;
+    
+        }
+    }
 }    
