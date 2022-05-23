01db1a9226ee7c3e80c64a8b72eeb3393b7601ef
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-05-23 10:55:25 +0300

    js// Creating a for loop to pick to random images

--- dice.js
+++ dice.js
@@ -8,3 +8,10 @@ let image_two=document.getElementsByClassName("img2")[0]
 // Declaring two variables to store the result of each player
 let player_one = 0
 let player_two= 0
+
+// For loop that etirates only 2 times in order to pick randomly only 2 images
+for (let i=0; i<2; i++){
+
+    let random_dice = dice_array[Math.floor(Math.random() * dice_array.length)]
+
+}    
